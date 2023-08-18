0508ab429bb88275d3e93fe0213e377bc0d88d40
DavidAshburn <oneinchwebbing@gmail.com>
2023-08-17 19:11:28 -0700

    working stop

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,11 +13,13 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         'inter': ['Inter Tight', 'sans-serif'],
         'abril': ['Abril Fatface', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
       },
       colors: {
         sitemain: '#214265',
         sitesecond: '#07a7cb',
         sitethird: '#00728b',
+        sitedark: '#092038',
         gold: '#efc634',
       },
       letterSpacing: {
